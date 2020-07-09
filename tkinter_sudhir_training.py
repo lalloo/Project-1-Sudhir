1bd8602dd8094000c115422ac10ab9e0fd2adc27
Sudhir Lalloo <65743040+lalloo@users.noreply.github.com>
2020-07-09 21:08:35 +0200

    Add files via upload
    Having learnt the basics in python and explored several libraries  I thought a good vision of within the code to see how GUI can be applied.
     Tkinter about the best library to make GUIs in python. 
    This basic example shows how you can capture details (EG Name, Contact and Address)
    Try this if you at the beginner level. Good foundation to understand basic GUI.

---
+++ tkinter_sudhir_training.py
@@ -0,0 +1,55 @@
+from tkinter import *
+
+window = Tk()
+window.config(bg = 'lightblue')
+window.title("Hello Python Friends")
+
+label1 = Label(window,text = "Greetings Python Members",bg= 'green',fg = 'white')
+window.geometry('500x500')
+
+def do():
+    name = name_entry.get()
+    contact = contact_entry.get()
+    address = address_entry.get()
+
+    window2 = Tk()
+
+    sdl1 = Label(window2,text = "Name:"+name)
+    sdl1.pack()
+    sdl2 = Label(window2,text = "Contact No:"+contact)
+    sdl2.pack()
+    sdl3 = Label(window2,text = "Address:"+address)
+    sdl3.pack()
+    window.destroy()
+    window2.mainloop()
+
+
+label1.pack()
+
+label2 = Label(window,text = "WELCOME TO PYTHON CLUB",bg='brown',fg="yellow",font=('arial',20,'bold'))
+label2.pack()
+
+name_label = Label(window,text = "Name")
+name_label.pack(anchor='w')
+
+name_entry = Entry(window)
+name_entry.pack(anchor='w')
+
+contact_label = Label(window,text = "Contact")
+contact_label.pack(anchor='w')
+
+contact_entry = Entry(window)
+contact_entry.pack(anchor='w')
+
+address_label = Label(window,text = "Address")
+address_label.pack(anchor='w')
+
+address_entry = Entry(window)
+address_entry.pack(anchor='w')
+
+Button1 = Button(window,text = "fetch this info",command = do)
+Button1.pack()
+
+window.mainloop()
+
+
